962408cc475865004b66f30246466719d7cc9201
Ben Hamner <ben@benhamner.com>
2016-12-04 07:04:11 -0800

    bugfixes

--- Makefile
+++ Makefile
@@ -7,10 +7,12 @@ working/no_header/papers.csv: output/papers.csv
 	mkdir -p working/no_header
 	tail +2 $^ > $@
 
-working/noHeader/authors.csv: working/no_header/papers.csv output/authors.csv
+working/no_header/authors.csv: output/authors.csv
+	mkdir -p working/no_header
 	tail +2 $^ > $@
 
-working/no_header/paper_authors.csv: working/no_header/papers.csv: output/paper_authors.csv 
+working/no_header/paper_authors.csv: output/paper_authors.csv
+	mkdir -p working/no_header
 	tail +2 $^ > $@
 
 output/database.sqlite: working/no_header/papers.csv working/no_header/paper_authors.csv working/no_header/authors.csv
--- src/nips_analysis.R
+++ src/nips_analysis.R
@@ -16,7 +16,19 @@ papers$char_count <- str_length(papers$paper_text)
 papers$no_ref_char_count <- str_length(papers$paper_text_no_refs)
 papers$body_proportion <- papers$no_ref_char_count/papers$char_count
 
-ggplot(papers, aes(x=papers$body_proportion)) + geom_histogram(bins=100)
+data_references <- papers[,"year"]
+for (dataset_name in c("mnist", "cifar", "imagenet")) {
+  data_references[dataset_name] <- 0
+  data_references[grep(dataset_name, papers$paper_text_no_refs, ignore.case = TRUE), dataset_name] <- 1
+}
+
+ggplot(data_references %>%
+  gather("dataset", "isreferenced", 2:ncol(data_references)) %>%
+  group_by(year, dataset) %>%
+  summarise(references=sum(isreferenced)), aes(x=year, y=references, group=dataset, color=dataset)) +
+  geom_line()
+
+ggplot(papers, aes(x=papers$body_proportion)) + geom_histogram(bins=100) 
 
 ggplot(papers, aes(x=year, y=char_count))+geom_point()
 
@@ -25,10 +37,9 @@ ggplot(papers, aes(x=char_count)) + geom_histogram(bins=100)
 grep("references", papers$paper_text, ignore.case=TRUE)
 
 grep("imagenet", papers$paper_text, ignore.case=TRUE)
-grep("imagenet", papers$paper_text, ignore.case=TRUE)
 grep("cifar", papers$paper_text, ignore.case=TRUE)
 
-papers[grep("kaggle", papers$paper_text, ignore.case=TRUE), "PdfName"]
+papers[grep("kaggle", papers$paper_text, ignore.case=TRUE), "title"]
 
 ggplot(papers %>% group_by(year) %>% summarise(count=n()), aes(x=year, y=count))+geom_point()
 
