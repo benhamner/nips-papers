13422b838791cd527e0eb45115fd8590fbaf2b66
Ben Hamner <ben@benhamner.com>
2017-03-28 15:07:19 -0700

    dont know

--- src/nips_analysis.R
+++ src/nips_analysis.R
@@ -16,17 +16,38 @@ papers$char_count <- str_length(papers$paper_text)
 papers$no_ref_char_count <- str_length(papers$paper_text_no_refs)
 papers$body_proportion <- papers$no_ref_char_count/papers$char_count
 
+dataset_names <- c("MNIST", "CIFAR", "SVHN", "PASCAL", "KITTI", "TFD", "SensIT", "Connect4", "Protein", "STL10",
+                 "adult", "credit", "kr-vs-kp", "promoters", "votes", "UCI", "digg", "HepTH",
+                 "citeseer", "MovieLens", "RocketFuel", "tweet", "twitter", "bAbI", "TreeBank", "Text8",
+                 "SARCOS", "NORB", "TIMIT", "ImageNet", "street", "Street View", "VGG", "Caltech-101",
+                 "FM-IQA", "AP News", "newsgroups", "diabetes", "HES", "prostate", "MS COCO",
+                 "Toronto Face", "glaucoma", "Alzheimer’s", "news20", "scleroderma",
+                 "puzzle", "MADELON", "ENRON", "WIPO", "reuters")
+
 data_references <- papers[,"year"]
-for (dataset_name in c("mnist", "cifar", "imagenet")) {
+for (dataset_name in dataset_names) {
   data_references[dataset_name] <- 0
-  data_references[grep(dataset_name, papers$paper_text_no_refs, ignore.case = TRUE), dataset_name] <- 1
+  matches <- grep(str_c(" ", dataset_name, "[^a-z]"), papers$paper_text_no_refs, ignore.case = TRUE)
+  data_references[matches, dataset_name] <- 1
 }
 
-ggplot(data_references %>%
+data_references_by_year <- data_references %>%
   gather("dataset", "isreferenced", 2:ncol(data_references)) %>%
   group_by(year, dataset) %>%
-  summarise(references=sum(isreferenced)), aes(x=year, y=references, group=dataset, color=dataset)) +
-  geom_line()
+  summarise(references=sum(isreferenced))
+
+popular_datasets <- unique(data_references_by_year[data_references_by_year$references>=10,]$dataset)
+popular_data_references_by_year <- data_references_by_year[data_references_by_year$dataset %in% popular_datasets,]
+
+ggplot(popular_data_references_by_year,
+       aes(x=year, y=references, group=dataset, color=dataset, label=dataset)) +
+  geom_line(size=2) +
+  geom_text(data=labels) +
+  theme_light(base_size=12)
+  
+  geom_text(data=popular_data_references_by_year[popular_data_references_by_year$year==2016,],
+            aes(x=year, y=references, label=dataset))
+
 
 ggplot(papers, aes(x=papers$body_proportion)) + geom_histogram(bins=100) 
 
